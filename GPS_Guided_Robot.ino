0fc33f8b4e6f835be288da5a954fcb4f698feb47
cty-saccharine <84186341+cty-saccharine@users.noreply.github.com>
2021-05-22 00:41:47 +0300

    Add files via upload

--- GPS_Guided_Robot.ino
+++ GPS_Guided_Robot.ino
@@ -2,15 +2,35 @@
 #include <QMC5883L.h>                                          
 #include <NMEAGPS.h>
 #include <GPSport.h>
-                      
+
+#define MIN(x,y) (x < y ? x : y)   //max min functions
+#define MAX(x,y) (x > y ? x : y)
+
+typedef struct {
+  double x, y;
+} Point;
+
+
+
+//******************************************************************************************************  
+// GPS Locations
+int ac =0;                                                         // GPS array counter
+int wpCount = 0;                                                   // GPS waypoint counter
+double Home_LATarray[50];                                          // variable for storing the destination Latitude - Only Programmed for 50 waypoint
+double Home_LONarray[50];                                          // variable for storing the destination Longitude - up to 50 waypoints
+
+
+int increment = 0;
 //******************************************************************************************************                                                                  
 // GPS Variables & Setup
 
 double lat,lon;
-int GPS_Course;                                                  // variable to hold the gps's determined course to destination
 int Number_of_SATS;                                                // variable to hold the number of satellites acquired
+float bearing;
+float distance;
 NMEAGPS  gps;
-gps_fix  fix;                                                      // gps = instance of NEOGPS 
+gps_fix  fix;                                                      // gps = instance of NEOGPS
+NeoGPS::Location_t to_waypoint(Home_LATarray[ac], Home_LONarray[ac]); 
                                                                    // pin 18 is connected to the TX on the GPS
                                                                    // pin 19 is connected to the RX on the GPS
 
@@ -30,10 +50,9 @@ int set_pwm = 200;                                               // motor pwm wh
 
 QMC5883L compass;                                                  // QMC5883L compass
 int compass_heading;
-//int16_t mx, my, mz;                                                // variables to store x,y,z axis from compass (HMC5883L)
 int desired_heading;                                               // initialize variable - stores value for the new desired heading                                             
 int compass_dev = 5;                                               // the amount of deviation that is allowed in the compass heading - Adjust as Needed
-int heading = 0;                                                                   // setting this variable too low will cause the robot to continuously pivot left and right
+int heading = 0;                                                   // setting this variable too low will cause the robot to continuously pivot left and right*****************************
                                                                    // setting this variable too high will cause the robot to veer off course
 
 //******************************************************************************************************
@@ -51,29 +70,56 @@ unsigned long previousMillis = 0;                                  // Store last
 const long interval = 200;                                         // Ping the Distance every X miliseconds
  
 //******************************************************************************************************
-// Bluetooth Variables & Setup    *********************GRUPLA İLETİŞİMİ KONUŞUP BURAYA ÖYLE BAK, NOKTALARI GÖNDERİRKEN BLUE. KULLANILMAYACAKSA SİL
+// Bluetooth Variables & Setup
 
-String str;                                                        // raw string received from android to arduino
-int blueToothVal;                                                  // stores the last value sent over via bluetooth
-int bt_Pin = 34;                                                   // Pin 34 of the Aruino Mega used to test the Bluetooth connection status - Not Used
+String veri;
+int v1;
+int v2;
+int v3;
+int v4;
+int v5;
+
+int v;
+
+String pp1;
+String pp2;
+String pp3;
+String pp4;
+String pp5;
+
+Point noktalar[5];
 
 //*****************************************************************************************************
-// GPS Locations
+//Pointing Variables & Setup
+int i;
+int a;
+int j;
+int k;
+int c=0;
 
-unsigned long Distance_To_Home;                                    // variable for storing the distance to destination
+double xmin;
+double xmax;
+double ymin;
+double ymax;
+// duruma göre kare dizisinin defaultu güncellenebilir
+Point alan[5];
+Point kare[1000];
+//polygon_ defaultu değişebilir.
+Point polygon_[250];
+//Point solu,sagu,sola,saga;
 
-int ac =0;                                                         // GPS array counter
-int wpCount = 0;                                                   // GPS waypoint counter
-double Home_LATarray[50];                                          // variable for storing the destination Latitude - Only Programmed for 50 waypoint
-double Home_LONarray[50];                                          // variable for storing the destination Longitude - up to 50 waypoints
+#define INSIDE 0
+#define OUTSIDE 1
 
+int N=250;
 
-int increment = 0;
+//*****************************************************************************************************
 
 void setup() 
 {  
-  Serial.begin(9600);                                           
-  gpsPort.begin( 9600 );
+  Serial.begin(9600);
+  Serial2.begin(9600);    //bluetooth için
+  gpsPort.begin(9600);
   
   while (!Serial)
     ;
@@ -92,6 +138,13 @@ void setup()
   compass.setRange(8);
 
   Startup();                                                       // Run the Startup procedure on power-up one time
+
+  //*****************************
+  Home_LATarray[1]=40692902;
+  Home_LONarray[1]=29833365;
+  Home_LATarray[2]=40692675;
+  Home_LONarray[2]=29833243;
+  //*****************************
 }
 
 //********************************************************************************************************
@@ -100,9 +153,9 @@ void setup()
 void loop()
 { 
   
-  bluetooth();                                                     // Run the Bluetooth procedure to see if there is any data being sent via BT                                                    
+  //bluetooth();                                                     // Run the Bluetooth procedure to see if there is any data being sent via BT                                                    
   getGPS();                                                        // Update the GPS location
   getCompass();                                                    // Update the Compass Heading
   //Ping();                                                        //*************************************** Collision Detection will have something to do with this. check later
-
+  goWaypoint();
 }
